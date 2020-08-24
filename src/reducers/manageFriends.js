29ef799dbb41e4d8ea64e6fb8edf8b8af7e1568c
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-24 19:38:05 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+        case "ADD_FRIEND":
+            let allFriends = Object.assign(...state, {friends: state.friends.concat(action.friend)})
+            return allFriends
+        case "REMOVE_FRIEND":
+            let remainingFriends = Object.assign(...state, {friends: state.friends.filter(friend => friend.id !== action.id)})
+            return remainingFriends
+        default:
+            return state;
+    }
 }
